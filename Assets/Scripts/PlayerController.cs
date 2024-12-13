69ddbbdf58f8981a7605b3179ba5e0adce19274d
bhusanSahoo <bhusan12sahoo@gmail.com>
2024-12-13 12:39:21 +0530

    Revert "problem in crouch animator"
    This reverts commit 7d8110da4d53fd8461474b955bd8787ff409e795.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 
@@ -54,22 +55,22 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("Crouch", crouch);
 
         Vector2 sizeofCollider = boxCollider.size;
-        Vector2 offsetofCollider = boxCollider.offset;
+        //Vector2 offsetofCollider = boxCollider.offset;
 
         if (crouch == true)
         {
 
             sizeofCollider = new Vector2(1.2f, 1.4f);
-            offsetofCollider = new Vector2(0f, 0.5f);
+            //offsetofCollider = new Vector2(0f, 0.5f);
         }
         else
         {
             sizeofCollider = new Vector2(0.6f, 2.2f);
-            offsetofCollider = new Vector2(0f, 1f);
+            //offsetofCollider = new Vector2(0f, 1f);
         }
 
         boxCollider.size = sizeofCollider;
-        boxCollider.offset = offsetofCollider;
+        boxCollider.offset = boxCollider.offset;
         //This updates the character’s size and direction in the game.
         //If scale.x = -1f, the character flips left.
         //If scale.x = 1f, the character flips right.
@@ -80,5 +81,8 @@ public class PlayerController : MonoBehaviour
         float jump = Input.GetAxisRaw("Vertical");
         animator.SetFloat("Jump", jump);
 
+
+
+        
     }
 }
