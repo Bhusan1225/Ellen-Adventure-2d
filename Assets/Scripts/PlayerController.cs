d87f68e1b88d4772aec55ed2bd7415aa0b7f4747
bhusanSahoo <bhusan12sahoo@gmail.com>
2024-12-13 10:01:06 +0530

    crouch is done
    done the resizing of the colllider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,12 @@ public class PlayerController : MonoBehaviour
 {
 
     public Animator animator;
+
+    public BoxCollider2D boxCollider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -18,20 +20,46 @@ public class PlayerController : MonoBehaviour
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
 
+        
+
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {
-            Debug.Log("speed < 0");
+           
             scale.x = -1f * Mathf.Abs(scale.x);
         }
         else
         {
-            Debug.Log("speed > 0");
+            
             scale.x = Mathf.Abs(scale.x);
 
         }
         
         transform.localScale = scale;
+        //This updates the character’s size and direction in the game.
+        //If scale.x = -1f, the character flips left.
+        //If scale.x = 1f, the character flips right.
+
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        animator.SetBool("Crouch", crouch);
+
+        Vector2 sizeofCollider = boxCollider.size;
+        Vector2 offsetofCollider = boxCollider.offset;
+
+        if (crouch == true)
+        {
+
+            sizeofCollider = new Vector2(1.2f, 1.4f);
+            offsetofCollider = new Vector2(0f, 0.5f);
+        }
+        else
+        {
+            sizeofCollider = new Vector2(0.6f, 2.2f);
+            offsetofCollider = new Vector2(0f, 1f);
+        }
+
+        boxCollider.size = sizeofCollider;
+        boxCollider.offset = boxCollider.offset;
         //This updates the character’s size and direction in the game.
         //If scale.x = -1f, the character flips left.
         //If scale.x = 1f, the character flips right.
