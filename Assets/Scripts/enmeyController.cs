710a7c87dab823ac5dc2ffb2e87e1cc7c667825e
bhusanSahoo <bhusan12sahoo@gmail.com>
2024-12-15 17:05:38 +0530

    added patrolling enemey done

--- Assets/Scripts/enmeyController.cs
+++ Assets/Scripts/enmeyController.cs
@@ -4,7 +4,39 @@ using UnityEngine;
 
 public class enmeyController : MonoBehaviour
 {
-    private Animator e_animator;
+
+    public float patrolSpeed = 2f;
+    public Vector3 pointA;
+    public Vector3 pointB;
+    private Vector3 targetPoint;
+    public Animator e_animator;
+
+    private void Start()
+    {
+        SetPatrolPoints();
+    }
+    
+    
+    private void Update()
+    {
+
+        PatrolEnmey();
+    }
+    private void SetPatrolPoints()
+    {
+        transform.position = pointA;
+        targetPoint = pointB;
+    }
+
+    private void PatrolEnmey()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPoint, patrolSpeed * Time.deltaTime);
+        if (transform.position == targetPoint)
+        {
+            targetPoint = (targetPoint == pointA) ? pointB : pointA;
+            e_animator.SetBool("Attack", true);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
@@ -15,7 +47,7 @@ public class enmeyController : MonoBehaviour
 
 
 
-            e_animator.SetBool("Attack", true);
+            //e_animator.SetBool("Attack", true);
 
 
 
